556c000c5b2d04d2dce4352bdf977ff19da7b054
SimonSun <yans3@illinois.edu>
2018-09-26 20:02:37 -0500

    yans3

--- src/App.js
+++ src/App.js
@@ -8,20 +8,30 @@ class App extends Component {
     todos: ["Walk to latea", "Get boba"]
   };
 
-  addParameter = () => {
+  adParameter = () => {
+    console.log("hi again");
     this.setState({
       todos: [...this.state.todos, "Get more boba"]
     })
   };
-
+  deleteParameter = () => {
+    console.log("hi");
+    this.setState({
+      todos: []
+    })
+  };
   render() {
     return (
       <div>
-        <h1>{this.state.todoListTitle}</h1>
+        <h1>{this.state.todoListTitle} </h1>
         {this.state.todos.map(x => (
+          <div>
+            <button onClick={this.deleteParameter}> Remove element</button>
           <ToDoListItem todo={x} />
+          </div>
         ))}
-        <button onClick={this.addParameter}> Add todo</button>
+        <button onClick={this.adParameter}> Add todo</button>
+
       </div>
     );
   }
